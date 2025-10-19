04017df171a91d7de682f11c2c57636d53a77afc
RhuanV <rhuan.vicente123@gmail.com>
2025-10-19 15:56:15 -0300

    gitignore e teste implementação dos ingredientes utilizando gerenciadorApp

--- classes/gerenciadorApp.py
+++ classes/gerenciadorApp.py
@@ -1,25 +1,50 @@
 from compra import Compra
 from venda import Venda
 from marmita import Marmita
-from ingrediente import Ingrediente
+from ingrediente import Ingrediente, FactoryIngrediente
 from gerenciadorBD import GerenciadorBD
 
 
-class GerenciadorApp:
+class GerenciadorApp():
+        
     def __init__(self):
-        self.FactoryVenda = FactoryVenda()
-        self.FactoryMarmita = FactoryMarmita()
-        self.FactoryIngrediente = FactoryIngrediente()
-        self.FactoryCompra = FactoryCompra()
-        self.GerenciadorBD = GerenciadorBD()
+        self.gerenciadorBD = GerenciadorBD()
+        ids = self.gerenciadorBD.getProximoID()
+
+        self.FactoryIngrediente = FactoryIngrediente(ids['ingredientes'])
+        # self.FactoryVenda = FactoryVenda(ids['vendas'])
+        # self.FactoryMarmita = FactoryMarmita(ids['marmitas'])
+        # self.FactoryCompra = FactoryCompra(ids['compras'])
 
         self.vendas = {}
         self.marmitas = {}
         self.ingredientes = {}
         self.compras = {}
 
-        #inicializar o banco de dados e carregar os dados existentes
-        
+        # Carregar dados do banco de dados na memória
+        self.LoadIngredientes()    
+        self.loadVendas()
+        self.loadMarmitas()
+        self.loadCompras()
+
+    def LoadIngredientes(self):
+        # Carrega ingredientes do banco de dados para a memória
+        ingredientesData = self.gerenciadorBD.getIngredientes()
+        for ingredienteDict in ingredientesData:
+            ingrediente = self.FactoryIngrediente.FromDB(ingredienteDict)
+            self.ingredientes[ingrediente.ID] = ingrediente
+
+    def loadVendas(self):
+        # Carrega vendas do banco de dados para a memória
+        pass
+
+    def loadMarmitas(self):
+        # Carrega marmitas do banco de dados para a memória
+        pass
+
+    def loadCompras(self):
+        # Carrega compras do banco de dados para a memória
+        pass
 
 
     def CreateVendas(self):
